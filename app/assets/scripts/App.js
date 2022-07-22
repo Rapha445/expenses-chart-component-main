2d498d545b8524661bf59375f4c29c3dc828875c
Rapha445 <raphaelle.delmas@lilo.org>
2022-07-22 14:04:18 +0900

    move data.json

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,15 @@ import 'lazysizes';
 import '../styles/styles.css';
 import ImportData from './modules/ImportData';
 
+fetch("data.json")
+.then((res) => {
+    console.log("Resolved", res);
+    res.json()
+})
+.catch((e) => {
+    console.log("Error", e);
+});
+
 if (module.hot) {
     module.hot.accept()
 };
