30149254eb71f6ef470c5a70b06896352f364729
Rapha445 <raphaelle.delmas@lilo.org>
2022-07-23 23:03:04 +0900

    js complete

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,25 +1,8 @@
 import 'lazysizes';
 import '../styles/styles.css';
 import ImportData from './modules/ImportData';
-import menu from '../../../data.json';
-import { indexOf } from 'lodash';
 
-console.log(menu);
-
-const bars = document.querySelectorAll(".week__daybar");
-
-const amounts = menu.map(object => object.amount);
-console.log(amounts);
-
-for (let i = 0; i < bars.length; i++) {
-    let index = i;
-    console.log(index);
-    console.log(bars[i]);
-    bars[i].style.height = `${amounts[index]}%`;
-}
-
-
-
+new ImportData();
 
 if (module.hot) {
     module.hot.accept()
--- app/assets/scripts/modules/ImportData.js
+++ app/assets/scripts/modules/ImportData.js
@@ -1,14 +1,36 @@
+import menu from '../../../../data.json';
 class ImportData {
     constructor() {
-        this.bar = document.querySelectorAll(".week__daybar");
+        this.bars = document.querySelectorAll(".week__daybar");
+        this.amounts = menu.map(object => object.amount);
+        this.maximum = 0;
+        this.menu = menu;
         this.events();
-        this.data = data;
     }
     events() {
-        this.window.addEventListener("load", () => this.loadBars())
+        window.addEventListener("load", () => this.loadBars())
     }
     loadBars() {
-        
+        for (let i = 0; i < this.bars.length; i++) {
+            this.calculateMax()
+            this.value = this.amounts[i]*100/this.maximum;
+            this.bars[i].style.height = `${this.value}%`;
+            this.focusMax();
+        }
+    }
+    calculateMax() {
+        for (let i = 0; i < this.amounts.length; i++) {
+            if (this.amounts[i] > this.maximum) {
+                this.maximum = this.amounts[i];
+            }
+        }
+    }
+    focusMax() {
+        for (let i = 0; i < this.bars.length; i++) {
+        if (this.amounts[i] === this.maximum) {
+            this.bars[i].classList.add("week__daybar--focus")
+        }
+    }
     }
 }
 
